2b4643a658d61f09256a66df43a0223bd66247d1
Amir Bazazi <amiralibazazi@gmail.com>
2014-10-13 15:19:17 +0100

    removed some unwanted components, added a button that shows/hides the keyboard on click and resizes the window

--- src/VirtualKeyboardExample.java
+++ src/VirtualKeyboardExample.java
@@ -2,69 +2,57 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class VirtualKeyboardExample extends Application {
 
-  @Override
-  public void start(Stage primaryStage) {
+    private boolean keyboardNotShown = true;
+
+    @Override
+  public void start(final Stage primaryStage) {
 
     final TextField textField = new TextField();
     textField.setOnAction(new EventHandler<ActionEvent>() {
-      @Override
-      public void handle(ActionEvent event) {
-        System.out.println("text field: "+textField.getText());
-      }
+        @Override
+        public void handle(ActionEvent event) {
+            System.out.println("text field: " + textField.getText());
+        }
     });
-    final TextArea textArea = new TextArea();
-    
-    final Button okButton = new Button("OK");
-    okButton.setDefaultButton(true);
-    okButton.setOnAction(new EventHandler<ActionEvent>() {
-      @Override
-      public void handle(ActionEvent event) {
-        System.out.println("OK Button Pressed!");
-      }
-    });
-    
-    final Button cancelButton = new Button("Cancel");
-    cancelButton.setCancelButton(true);
-    cancelButton.setOnAction(new EventHandler<ActionEvent>() {
-      @Override
-      public void handle(ActionEvent event) {
-        System.out.println("Canceled!");
-      }
-    });
-    
-    final CheckBox disabledCheckBox = new CheckBox("Disable");
-    
-    final HBox buttons = new HBox(5);
-    buttons.getChildren().addAll(okButton, cancelButton, disabledCheckBox);
-    buttons.setAlignment(Pos.CENTER);
 
-    final VBox root = new VBox(5);
-    root.setPadding(new Insets(10));
-    Scene scene = new Scene(root);
-    
-    VirtualKeyboard vkb = new VirtualKeyboard();
-    
-    // just add a border to easily visualize the boundary of the keyboard:
-    vkb.view().setStyle("-fx-border-color: darkblue; -fx-border-radius: 5;");
-    vkb.view().disableProperty().bind(disabledCheckBox.selectedProperty());
-    
-    root.getChildren().addAll(textField, textArea, buttons, vkb.view());
 
-    primaryStage.setScene(scene);
-    primaryStage.show();
-  }
+        final VBox root = new VBox(5);
+        root.setPadding(new Insets(10));
+        Scene scene = new Scene(root);
+
+        final VirtualKeyboard vkb = new VirtualKeyboard();
+
+        // just add a border to easily visualize the boundary of the keyboard:
+        vkb.view().setStyle("-fx-border-color: darkblue; -fx-border-radius: 5;");
+        final Button showKeyboardButton = new Button("Show Keyboard!");
+        showKeyboardButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if (keyboardNotShown) {
+                    root.getChildren().add(vkb.view());
+                    primaryStage.sizeToScene();
+                } else {
+                    root.getChildren().remove(vkb.view());
+                    primaryStage.sizeToScene();
+                }
+                keyboardNotShown = !keyboardNotShown;
+            }
+        });
+
+        root.getChildren().addAll(textField, showKeyboardButton);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
 
   public static void main(String[] args) {
     launch(args);
